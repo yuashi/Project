b8268c48f8618f93b2739e6d84954bd76b0ad691
Ayushi Singh <singh9ayushi9@gmail.com>
2021-02-24 18:29:01 +0530

    changed the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,185 +1,88 @@
-import React,{Component} from 'react';
-import {Navbar,NavbarBrand,Nav,NavbarToggler,Collapse,NavItem,Button,Modal,ModalHeader,ModalBody,
-    Form,FormGroup,Input ,Label,NavLink,Jumbotron,Container,InputGroup, InputGroupAddon} from 'reactstrap';
-import  { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faSignInAlt,faSearchLocation} from '@fortawesome/free-solid-svg-icons';
-/*import { Input, Menu } from 'semantic-ui-react'*/
+import React,{useState} from 'react';
+import {Icon,Menu,Input,Button,Checkbox,Form,Image} from 'semantic-ui-react';
+import {Jumbotron,Container,Modal,ModalHeader,ModalBody,ModalFooter} from 'reactstrap';
 
+const Header =()=>{
 
-class Header extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            isNavOpen:false,
-            isModalOpen:false,
-            activeItem:'home',
-        };
-        this.toggleNav=this.toggleNav.bind(this);
-        this.toggleModal=this.toggleModal.bind(this);
-        this.handleLogin=this.handleLogin.bind(this);
-        /*this.handleItemClick=this.handleItemClick.bind(this);*/
-    }
-    /*handleItemClick = (e, { name }) => this.setState({ activeItem: name })*/
+    const[activeItem,setActiveItem]=useState('home');
+    const[open,setOpen]=useState(false);
 
-    toggleModal(){
-        this.setState({
-            isModalOpen:!this.state.isModalOpen
-        });
-    }
-
-    toggleNav(){
-        this.setState({
-            isNavOpen:!this.state.isNavOpen
-        });
-    }
-
-    handleLogin(event){
-        this.toggleModal();
-        alert("Username: "+this.username.value + " Password: "+this.password.value + " Remember: " + this.remember.checked);
-        event.preventDefault();
-
-    }
-
-    render(){
-        /*const { activeItem } = this.state*/
-        return(
-            <>
-           {/* <Menu secondary>
+    const handleItemClick=(e,{name})=>setActiveItem(name);
+    const toggleModal=()=>setOpen(!open);
+    
+    return(
+        <>
+        <Menu icon='labeled' inverted fixed='top' fluid widths={6} stackable borderless>
+        <Menu.Item header><Image src="assets/imgs/logo.jpg" size='tiny' verticalAlign='top' floated='left' /></Menu.Item>
+        <Menu.Item >
+        <Input
+        size='mini'
+        action={{ type: 'submit', content: 'Go' }}
+        placeholder='Search'
+        />
+        </Menu.Item>
         <Menu.Item
           name='home'
           active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-        />
+          onClick={handleItemClick}
+        >
+        <Icon name='home'/>
+        Home
+        </Menu.Item>
         <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          onClick={this.handleItemClick}
-        />
+          name='aboutus'
+          active={activeItem === 'aboutus'}
+          onClick={handleItemClick}
+        >
+        <Icon name='info'/>
+        About Us
+        </Menu.Item>
         <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Menu position='right'>
-          <Menu.Item>
-            <Input icon='search' placeholder='Search...' />
-          </Menu.Item>
-          <Menu.Item
-            name='login'
-            active={activeItem === 'logout'}
-            onClick={this.toggleModal}
-          />
-        </Menu.Menu>
-           </Menu>*/}
+          name='services'
+          active={activeItem === 'services'}
+          onClick={handleItemClick}
+        >
+        <Icon name='services'/>
+        Services
+        </Menu.Item>
+        <Menu.Item>
+        <Button onClick={toggleModal}>Log-in</Button>
+        </Menu.Item>    
+        </Menu>
 
+<div>
+<Jumbotron fluid style={{backgroundImage:`url('assets/imgs/tourist.jpg')`,backgroundSize:'cover',color:'white',marginTop:50}}>
+    <Container fluid >
+        <h1 className="display-1">TravelPal</h1>
+        <p className="lead">You are not alone! Explore India with our best Guides and get the most of your travel.<br/>
+         Happy Exploring !</p>
+    </Container>
+</Jumbotron>
+</div>
 
-            <Navbar dark color="dark" expand="md" fixed="top" className="justify-content-between" >
-    
-                   
-                  <NavbarBrand  href="/">
-                  <img src="assets/imgs/logo.jpg" height="40" width="60"
-                           alt="TravelPal" /> TravelPal
-                  </NavbarBrand>
-                  <NavbarToggler onClick={this.toggleNav} />
-                  <NavItem className="navitems">
-                      <div style={{display:"inline-block",alignItems:"center",justifyContent:"center",left:0}}>
-                      <SearchBar/>
-                      </div>
-                  </NavItem>
-                  <Collapse isOpen={this.state.isNavOpen} navbar>
-                  <Nav navbar className="ml-auto">
-                      <NavItem className="navitems">
-                          <NavLink href="#">
-                              <span className="fa fa-home fa-lg"></span> Home
-                          </NavLink>
-                     </NavItem>  
-                     <NavItem className="navitems">
-                          <NavLink href="#">
-                              <span className="fa fa-info fa-lg"></span> About Us
-                          </NavLink>
-                      </NavItem>
-                      <NavItem className="navitems">
-                          <NavLink href="#">
-                              <span className="fa fa-list fa-lg"></span> Services
-                          </NavLink>
-                      </NavItem>
-                      <NavItem className="navitems">
-                          <NavLink href="#">
-                              <span className="fa fa-address-card fa-lg"></span> Contact Us
-                          </NavLink>
-                       </NavItem>   
-                  </Nav>
-                  </Collapse>
-                  <Nav className="mr-auto" navbar>
-                      <NavItem>
-                          <Button  color="secondary"  onClick={this.toggleModal}>
-                          <FontAwesomeIcon icon={faSignInAlt}/> Login
-                          </Button>
-                      </NavItem>
-                  </Nav>
-               
-        </Navbar>
-               
-               <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                   <ModalHeader className="modalheader" toggle={this.toggleModal}>Login</ModalHeader>
-                   <ModalBody>
-                       <Form onSubmit={this.handleLogin}>
-                           <FormGroup>
-                               <Label htmlFor="username">Username</Label>
-                               <Input type="text" id="username" name="username" 
-                                innerRef={(input) => this.username=input}/>
-                           </FormGroup>
-                           <FormGroup>
-                               <Label htmlFor="password">Password</Label>
-                               <Input type="password" id="password" name="password" 
-                                innerRef={(input) => this.password=input}/>
-                           </FormGroup>
-                           <FormGroup check>
-                               <Label check>
-                                   <Input type="checkbox" name="remember" 
-                                    innerRef={(input) => this.remember=input}/>
-                                   Remember me
-                               </Label>
-                           </FormGroup>
-                           <Button type="submit" value="submit" color="primary" >Login</Button>
-                           <hr/>
-                           <FormGroup>
-                               Coming for the first time ?<br/>
-                           </FormGroup>
-                           <Button  type="submit" value="SignUp" color="warning">Sign Up</Button>
-                       </Form>
+     <Modal
+      isOpen={open} toggle={toggleModal}>
+    <ModalHeader className="modalheader" toggle={toggleModal}>Login</ModalHeader>
+    <ModalBody>  
+    <Form onSubmit={toggleModal}>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </Form.Field>
+    <Button type='submit' fluid color='blue'>Submit</Button>
+    </Form>
+    </ModalBody>
+    <ModalFooter><Button onClick={toggleModal} fluid color='yellow'>Sign Up</Button></ModalFooter> 
+     </Modal>
 
-                   </ModalBody>
-               </Modal>
-
-               <div>
-                   <Jumbotron fluid style={{backgroundImage:`url('assets/imgs/tourist.jpg')`,backgroundSize:'cover',color:'white',marginTop:50}}>
-                       <Container fluid >
-                           <h1 className="display-1">TravelPal</h1>
-                           <p className="lead">You are not alone! Explore India with our best Guides and get the most of your travel.<br/>
-                            Happy Exploring !</p>
-                       </Container>
-                   </Jumbotron>
-               </div>
-               </>
-
-            
-        );
-    }
-}
-
- const SearchBar = () => {
-    return(
-       
-            <InputGroup className="mb-2" >
-                <Input placeholder="Where do you want to go ?" />
-                <InputGroupAddon addonType="append">
-                <Button outline color="secondary"><FontAwesomeIcon icon={faSearchLocation}/></Button>
-                </InputGroupAddon>
-    </InputGroup>
-    
-    
-       
+</>
     );
 }
-export default  Header;
+export default Header;
